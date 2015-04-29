aba45cf144a69cb37ce391773f91afefe71b7feb
Mikey <mikey.shulman@gmail.com>
2015-04-28 22:46:29 -0400

    adding new files for windows form

---
+++ PPTAddIn/SlideTracker/TrackerForm.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace SlideTracker
+{
+    partial class TrackerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.OK_button = new System.Windows.Forms.Button();
+            this.cancel_button = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Tag = "label1";
+            this.label1.Text = "SlideTracker: ";
+            // 
+            // OK_button
+            // 
+            this.OK_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OK_button.Location = new System.Drawing.Point(207, 217);
+            this.OK_button.Name = "OK_button";
+            this.OK_button.Size = new System.Drawing.Size(123, 67);
+            this.OK_button.TabIndex = 1;
+            this.OK_button.Tag = "OK_button";
+            this.OK_button.Text = "OK";
+            this.OK_button.UseVisualStyleBackColor = true;
+            this.OK_button.Click += new System.EventHandler(this.OK_button_Click);
+            // 
+            // cancel_button
+            // 
+            this.cancel_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cancel_button.Location = new System.Drawing.Point(37, 217);
+            this.cancel_button.Name = "cancel_button";
+            this.cancel_button.Size = new System.Drawing.Size(123, 67);
+            this.cancel_button.TabIndex = 2;
+            this.cancel_button.Tag = "cancel_button";
+            this.cancel_button.Text = "Cancel";
+            this.cancel_button.UseVisualStyleBackColor = true;
+            this.cancel_button.Click += new System.EventHandler(this.cancel_button_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.Location = new System.Drawing.Point(34, 133);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(69, 16);
+            this.linkLabel1.TabIndex = 3;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "linkLabel1";
+            this.linkLabel1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(37, 165);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(308, 23);
+            this.progressBar.TabIndex = 4;
+            // 
+            // TrackerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(371, 296);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.cancel_button);
+            this.Controls.Add(this.OK_button);
+            this.Controls.Add(this.label1);
+            this.Name = "TrackerForm";
+            this.Text = "SlideTracker";
+            this.Load += new System.EventHandler(this.TrackerForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OK_button;
+        private System.Windows.Forms.Button cancel_button;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ PPTAddIn/SlideTracker/TrackerForm.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SlideTracker
+{
+    public partial class TrackerForm : Form
+    {
+        public bool done = false;
+        public bool cancelledForm = false;
+        public TrackerForm()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+            this.linkLabel1.Visible = false;
+            this.label1.MaximumSize = new Size(250, 0);
+            this.label1.AutoSize = true;
+        }
+
+        private void OK_button_Click(object sender, EventArgs e)
+        {
+            if (this.done) { this.Close(); }
+        }
+
+        private void cancel_button_Click(object sender, EventArgs e)
+        {
+            this.cancelledForm = true;
+            this.Focus();
+            this.TopMost = true;
+            this.Refresh();
+            System.Windows.Forms.MessageBox.Show("cancelled");
+        }
+
+        public void ChangeLabelText(string text)
+        {
+            this.label1.Text = text;
+            this.FormRefresh();
+        }
+
+        public void InitProgressBar(int nSlides)
+        {
+            this.progressBar.Maximum = nSlides;
+            this.progressBar.Step = 1;
+        }
+
+        public void UpdateProgressBar()
+        { 
+            this.progressBar.PerformStep();
+            this.FormRefresh();
+        }
+
+        public void FormRefresh()
+        {
+            this.Focus();
+            this.TopMost = true;
+            this.Refresh();
+        }
+
+        public void DisplayLinkLabel(string text)
+        {
+            this.linkLabel1.Text = text;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(LinkClicked);
+            this.linkLabel1.VisitedLinkColor = System.Drawing.Color.Blue;
+            //this.linkLabel1.LinkColor = System.Drawing.Color.Navy;
+            this.linkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
+            this.linkLabel1.Visible = true;
+        }
+
+        private void LinkClicked(object sender, System.Windows.Forms.LinkLabelLinkClickedEventArgs e) //callback for clicking link
+        {
+            System.Diagnostics.Process.Start(Globals.ThisAddIn.GetLinkURL());
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void TrackerForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
